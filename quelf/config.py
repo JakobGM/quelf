1e92f56c235bf22a9fb5e1467499f9e42ff32ad9
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-04-13 12:04:41 +0200

    deps: Upgrade dependencies

--- quelf/config.py
+++ quelf/config.py
@@ -2,6 +2,10 @@ from pathlib import Path
 from typing import Optional
 
 import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 
 CONFIG_DIRECTORY = Path(__file__).parents[1]
@@ -12,7 +16,7 @@ DATA_DIRECTORY = CONFIG_DIRECTORY / 'data'
 class Config:
     def __init__(self, config_file: Path = CONFIG_FILE) -> None:
         with open(config_file, 'r') as file:
-            self.yaml = yaml.load(file)
+            self.yaml = yaml.load(file, Loader=Loader)
 
     def __getitem__(self, key):
         return self.yaml[key]
